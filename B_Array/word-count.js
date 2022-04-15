6ed92fc534d0be0eba573f668662af49275275b3
mashiyyat15 <andriambolatsiory@gmail.com>
2022-04-15 15:06:43 +0300

    go on

--- B_Array/word-count.js
+++ B_Array/word-count.js
@@ -1,7 +1,20 @@
 // Ecrivez une fonction `wordCount(sentence, targetWords)` qui accepte une chaîne de phrase et un tableau de mots cibles. 
 
 // La fonction doit retourner un compte du nombre de mots de la phrase qui se trouvent dans `targetWords`.
-
+function wordCount(str1,str2){
+    let nombre_trouve=0;
+    for (i=0;i<str2.length;i++){
+        var letter=str2[i];
+        let trouve=str1.includes(letter);
+        if (trouve==true){
+            nombre_trouve+=1;
+        }
+        else if(trouve==false){
+            nombre_trouve+=0;
+        }
+    }
+    return nombre_trouve
+}
 
 console.log(wordCount("ouvrez la fenêtre s'il vous plaît", ["s'il vous plaît", "ouvrir", "désolé"])) ; // 2
 console.log(wordCount("conduire au cinéma", ["le", "conducteur"])) ; // 1
